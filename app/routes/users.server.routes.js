ec9c96d0a5a8470b905fcd56d7b807248253d566
aneesh kulkarni <askthefactorcamera@gmail.com>
2019-07-05 01:30:08 +0530

    Minor Changes

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,4 +1,4 @@
 var users = require('../../app/controllers/users.server.controller');
 module.exports = function(app){
-    app.route('/users').post(users.create);
+    app.route('/users').post(users.create).get(users.list);
 };
