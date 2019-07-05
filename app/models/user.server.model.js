2b2fd0d5a3a39067d0b41e47f604ad3185d8875a
aneesh kulkarni <askthefactorcamera@gmail.com>
2019-07-05 17:58:51 +0530

    Schema Modifiers and Custom Model methods added

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -3,8 +3,51 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: String,
-    username: String,
-    password: String
+    email: {
+        type: String,
+        index: true,
+        match: /.+\@.+\..+/
+    },
+    username: {
+        type: String,
+        trim:true,
+        unique:true,
+        required: true
+    },
+    role:{
+        type: String,
+        enum: ['Admin', 'Owner', 'User']
+    },
+
+    password: {
+        type: String,
+        validate: [function(password){
+            return password.length >= 6;
+        }, "Password should be longer"]
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    website:{
+        type:String,
+        get: function(url) {
+            if(!url){
+                return url;
+            } else {
+                if(url.indexOf('http://')!==0 && url.indexOf('https://')!==0){
+                    url = 'http://' +url;
+                }
+                return url;
+            }
+        }
+    }
 });
+UserSchema.statics.findOneByUsername = function(username, callback){
+    this.findOne({username: new RegExp(username, 'i')}, callback);
+};
+UserSchema.virtual('fullName').get(function(){
+    return this.firstName + ' ' + this.lastName;
+});
+UserSchema.set('toJSON',{getters:true, virtuals: true});
 module.exports = {conn: mongoose.model('User', UserSchema)}
